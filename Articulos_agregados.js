6680991aa031f4d78e6f15a618aedc4898788b1e
RoyaltyAJ <44954672+RoyaltyAJ@users.noreply.github.com>
2020-10-16 12:08:28 -0400

    Add files via upload

--- Articulos_agregados.js
+++ Articulos_agregados.js
@@ -1,3 +1,15 @@
+var nombre = document.getElementById('NOMBRE DEL PUBLICANTE');
+var apellido = document.getElementById('APELLIDO DEL PUBLICANTE');
+var titulo = document.getElementById('TITULO');
+var carrera = document.getElementById('CARRERA');
+
+nombre.innerText = localStorage.getItem('nombre');
+
+alert("el nombre del publicante es:" + nombre);
+alert("apellido: " + apellido);
+
+console.log("el nombre del publicante es:" + nombre);
+
 localStorage.getItem('nombre');
 localStorage.getItem('apellido');
 localStorage.getItem('titulo');
